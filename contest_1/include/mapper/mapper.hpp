1872ea96be80cf86f9c7daca65c6e753611f5f83
turtlebot <jihad.elsheikh@mail.utoronto.ca>
2018-01-27 19:03:16 -0500

    Processing the gmap layer to create a local map

--- contest_1/include/mapper/mapper.hpp
+++ contest_1/include/mapper/mapper.hpp
@@ -5,6 +5,9 @@
 #include<sensor_msgs/LaserScan.h>
 #include<geometry_msgs/Point.h>
 #include<nav_msgs/OccupancyGrid.h>
+#include<vector>
+
+using namespace std;
 
 namespace goofy{
 namespace mapper{
@@ -17,12 +20,31 @@ public:
 private:
 	void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
 
-	std::vector<geometry_msgs::Point> obstacle_positions;
+	vector<geometry_msgs::Point> obstacle_positions;
 
 	ros::NodeHandle _nh;
 	ros::Subscriber _laser_sub;
+};
 
+struct LocalMap {
+	// map
+	vector<vector<int> > matrix;
+	double resolution;
+	int boundingHeight;
+	int boundingWidth;
+	geometry_msgs::Pose origin;
+	ros::Time time;
+};
 
+class GMap{
+public:
+	GMap(){}
+	LocalMap processRawMap(const nav_msgs::OccupancyGrid grid);
+private:
+	int getMinHeight(int width, int height, std::vector<int8_t> data);
+	int getMaxHeight(int width, int height, std::vector<int8_t> data);
+	int getMinWidth(int width, int height, std::vector<int8_t> data);
+	int getMaxWidth(int width, int height, std::vector<int8_t> data);
 };
 
 }
--- contest_1/src/mapper/mapper.cpp
+++ contest_1/src/mapper/mapper.cpp
@@ -1,94 +1,121 @@
 #include "mapper/mapper.hpp"
 
-namespace goofy{
-namespace common{
+namespace goofy {
+namespace mapper {
 
-void processRawMap(const nav_msgs::OccupancyGrid grid){
-  int width = grid.info.width;
-  int height = grid.info.height;
+mapper::LocalMap GMap::processRawMap(const nav_msgs::OccupancyGrid grid) {
+	int width = grid.info.width;
+	int height = grid.info.height;
 
-  int minWidth = 0;
-  int maxWidth = 0;
+	int minWidth;
+	int maxWidth;
 
-  int minHeight = 0;
-  int maxHeight = 0;
+	int minHeight;
+	int maxHeight;
 
-  int boundingWidth = 0;
-  int boundingHeight = 0;
+	int boundingWidth = 0;
+	int boundingHeight = 0;
 
-  bool found = false;
-  for (int i=0; i<height; i++) {
-    for (int j=0; j<width; j++) {
-      int index = i*width+j;
-      if (grid.data[index] >= 0){
-        minHeight = i;
-        found = true;
-        break;
-      }
-    }
-    if (found) {
-      break;
-    }
-  }
+	// get a bounding box around the known sections of the map
+	minHeight = GMap::getMinHeight(width, height, grid.data);
+	maxHeight = GMap::getMaxHeight(width, height, grid.data);
+	minWidth = GMap::getMinWidth(width, height, grid.data);
+	maxWidth = GMap::getMaxWidth(width, height, grid.data);
 
-  found = false;
-  for (int i=height-1; i>=0; i--) {
-    for (int j=0; j<width; j++) {
-      int index = i*width+j;
-      if (grid.data[index] >= 0){
-        maxHeight = i;
-        found = true;
-        break;
-      }
-    }
-    if (found) {
-      break;
-    }
-  }
+//	mapper::LocalMap localMap = new mapper::LocalMap;
+	mapper::LocalMap localMap = {};
 
-  found = false;
-  for (int i=0; i<width; i++) {
-    for (int j=0; j<height; j++) {
-      int index = j*width+i;
-      if (grid.data[index] >= 0){
-        minWidth = i;
-        found = true;
-        break;
-      }
-    }
-    if (found) {
-      break;
-    }
-  }
+	// if any of them is -1 (then all should be -1) and it means nothing is known
+	if (minHeight < 0 || maxHeight < 0 || minWidth < 0 || maxWidth < 0) {
+		return localMap;
+	}
 
-  found = false;
-  for (int i=width-1; i>=0; i--) {
-    for (int j=0; j<height; j++) {
-      int index = j*width+i;
-      if (grid.data[index] >= 0){
-        maxWidth = i;
-        found = true;
-        break;
-      }
-    }
-    if (found) {
-      break;
-    }
-  }
-  boundingHeight = maxHeight - minHeight + 1;
-  boundingWidth = maxWidth - minWidth + 1;
+	// get the size of the bounding box
+	boundingHeight = maxHeight - minHeight + 1;
+	boundingWidth = maxWidth - minWidth + 1;
 
-  int robotPosX = grid.info.origin.position.x;
+	// copy the bounded region to a new array
+//	int matrix[boundingHeight][boundingWidth];
+	vector<vector<int> > matrix;
+	matrix.resize(boundingHeight);
+	for (int i = 0; i < boundingHeight; i++) {
+		matrix[i].resize(boundingWidth);
+		for (int j = 0; j < boundingWidth; j++) {
+			int index = (i + minHeight) * width + (j + minWidth);
+			matrix[i][j] = grid.data[index];
+		}
+	}
 
+	geometry_msgs::Pose pose = grid.info.origin;
+	// THIS IS NOT CORRECT. nEED TO CONSIDER ORIENTATION AS WELL
+	pose.position.x += minWidth * grid.info.resolution;
+	pose.position.y += minHeight * grid.info.resolution;
 
+	localMap.boundingHeight = boundingHeight;
+	localMap.boundingHeight = boundingHeight;
+	localMap.matrix = matrix;
+	localMap.resolution = grid.info.resolution;
+	localMap.time = grid.info.map_load_time;
+	localMap.origin = pose;
 
-  ROS_INFO("I have a grid");
-  ROS_INFO("%i %i %f",grid.info.width, grid.info.height, grid.info.resolution);
-  ROS_INFO("%f %f %f",grid.info.origin.position.x, grid.info.origin.position.y, grid.info.origin.position.z);
-  ROS_INFO("calculated width height");
-  ROS_INFO("%i %i", boundingWidth, boundingHeight);
-  //int8[] data = grid.data;
-  //ROS_INFO();
+	ROS_INFO("I have a grid");
+	ROS_INFO("%i %i %f", grid.info.width, grid.info.height,
+			grid.info.resolution);
+	ROS_INFO("%f %f %f", grid.info.origin.position.x,
+			grid.info.origin.position.y, grid.info.origin.position.z);
+	ROS_INFO("calculated width height");
+	ROS_INFO("%i %i", boundingWidth, boundingHeight);
+	//int8[] data = grid.data;
+	//ROS_INFO();
+	return localMap;
+}
+
+int GMap::getMinHeight(int width, int height, vector<int8_t> data) {
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			int index = i * width + j;
+			if (data[index] >= 0) {
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+int GMap::getMaxHeight(int width, int height, vector<int8_t> data) {
+	for (int i = height - 1; i >= 0; i--) {
+		for (int j = 0; j < width; j++) {
+			int index = i * width + j;
+			if (data[index] >= 0) {
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+int GMap::getMinWidth(int width, int height, vector<int8_t> data) {
+	for (int i = 0; i < width; i++) {
+		for (int j = 0; j < height; j++) {
+			int index = j * width + i;
+			if (data[index] >= 0) {
+				return i;
+			}
+		}
+	}
+	return -1;
+}
+
+int GMap::getMaxWidth(int width, int height, vector<int8_t> data) {
+	for (int i = width - 1; i >= 0; i--) {
+		for (int j = 0; j < height; j++) {
+			int index = j * width + i;
+			if (data[index] >= 0) {
+				return i;
+			}
+		}
+	}
+	return -1;
 }
 
 }
