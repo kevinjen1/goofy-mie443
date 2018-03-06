11a23e1f8a043d1b49b860e56d227de69e37a575
Jihad El Sheikh <jihadzak@hotmail.com>
2018-03-06 18:27:03 -0500

    adding back drawing the matches

--- contest_2/src/contest2.cpp
+++ contest_2/src/contest2.cpp
@@ -214,7 +214,39 @@ int main(int argc, char** argv){
           		
           		std::cout << "NumOfMatches: " << bestMatch << std::endl;
           		std::cout << "Pic is:" << foundPic << std::endl;
+			/**	
+				Mat img_matches;
+ 		drawMatches( img_object, keypoints_object, img_scene, keypoints_scene, good_matches, img_matches, Scalar::all(-1), Scalar::all(-1), vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
 
+  		//-- Localize the object
+  		std::vector<Point2f> obj;
+  		std::vector<Point2f> scene;
+  		for(int i = 0; i < good_matches.size(); i++){
+			//-- Get the keypoints from the good matches
+			obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+			scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+  		}
+
+  		Mat H = findHomography( obj, scene, RANSAC );
+  		//-- Get the corners from the image_1 ( the object to be "detected" )
+  		std::vector<Point2f> obj_corners(4);
+  		obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
+  		obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
+  		std::vector<Point2f> scene_corners(4);
+  		perspectiveTransform( obj_corners, scene_corners, H);
+
+  		//-- Draw lines between the corners (the mapped object in the scene - image_2 )
+  		line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0),
+  		scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
+  		line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0),
+  		scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+  		line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0),
+  		scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+  		line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0),
+  		scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+  		//-- Show detected matches
+  		imshow( "Good Matches & Object detection", img_matches );
+*/
    			    std::cout << "releasting video" << endl;
    			    video.release();
    			} else {
