f8769ccc7c04602acb4f5d3849db6705152c087f
turtlebot <jihadzak@hotmail.com>
2018-03-08 16:05:24 -0500

    Adding unit test but not working

---
+++ contest_2/test/test_pic_sim.cpp
@@ -0,0 +1,16 @@
+#include<gtest/gtest.h>
+#include<ros/ros.h>
+#include<chrono>
+#include<thread>
+
+
+TEST(PrimitiveTests, PathGeneration){
+	ROS_INFO("hello there");
+}
+
+int main(int argc, char **argv){
+	ros::init(argc, argv, "test_pic_sim", ros::init_options::AnonymousName);
+	testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
