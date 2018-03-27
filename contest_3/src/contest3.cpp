87408fad27774c42f4d92d2621035c31eb2b8afc
Bean Bean <bean@gmail.com>
2018-03-27 19:56:04 -0400

    Launch file changes for contest 3 - not finished, but wont error

--- contest_3/src/contest3.cpp
+++ contest_3/src/contest3.cpp
@@ -1,8 +1,10 @@
 #include <header.h>
 #include <ros/package.h>
 #include <imageTransporter.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 geometry_msgs::Twist follow_cmd;
 int world_state;
@@ -23,6 +25,7 @@ int main(int argc, char **argv)
 	ros::NodeHandle nh;
 	sound_play::SoundClient sc;
 	string path_to_sounds = ros::package::getPath("mie443_contest3") + "/sounds/";
+	string path_to_videos = ros::package::getPath("mie443_contest3") + "/videos/";
 	teleController eStop;
 
 	//publishers
@@ -47,6 +50,44 @@ int main(int argc, char **argv)
 
 	sc.playWave(path_to_sounds + "sound.wav");
 	ros::Duration(0.5).sleep();
+	
+	//-- GRACE - VIEDO + SOUND
+	/*
+	sc.playWave(path_to_sounds + "shocked3.wav");
+	
+	VideoCapture cap(path_to_videos + "shocked3.mp4"); 
+    
+  // Check if camera opened successfully
+  if(!cap.isOpened()){
+    cout << "Error opening video stream or file" << endl;
+    return -1;
+  }
+  //sc.playWave(path_to_sounds + "shocked3.wav");
+  while(1){
+ 
+    Mat frame;
+    // Capture frame-by-frame
+    cap >> frame;
+  
+    // If the frame is empty, break immediately
+    if (frame.empty())
+      break;
+ 
+    // Display the resulting frame
+    imshow( "Frame", frame );
+ 
+    // Press  ESC on keyboard to exit
+    char c=(char)waitKey(25);
+    if(c==27)
+      break;
+  }
+  
+  // When everything done, release the video capture object
+  cap.release();
+ 
+  // Closes all the frames
+  destroyAllWindows(); */
+  // GRACE -- VIDEO AND SOUND
 
 	while(ros::ok()){
 		ros::spinOnce();
@@ -54,6 +95,7 @@ int main(int argc, char **argv)
 		//eStop.block();
 		//...................................
 
+        std::cout << follow_cmd << std::endl;                               
 		if(world_state == 0){
 			//fill with your code
 			//vel_pub.publish(vel);
